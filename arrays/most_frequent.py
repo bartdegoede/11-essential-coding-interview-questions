0c199a694c3b4a48f8e53713296b4aaff7d19997
Bart de Goede <b.de.goede@catawiki.nl>
2018-06-03 21:28:46 +0200

    Keep track of max value, so we don't have to do another iteration on the dict

--- arrays/most_frequent.py
+++ arrays/most_frequent.py
@@ -2,12 +2,17 @@ def most_frequent(given_list):
     if not given_list:
         return None
 
+    max_item = None
+    max_count = -1
     frequencies = {}
     for item in given_list:
         if item not in frequencies:
             frequencies[item] = 0
         frequencies[item] += 1
-    return max(frequencies.items(), key=lambda x: x[1])[0]
+        if frequencies[item] > max_count:
+            max_count = frequencies[item]
+            max_item = item
+    return max_item
 
 if __name__ == '__main__':
     assert(most_frequent([1, 3, 1, 3, 2, 1]) == 1)
