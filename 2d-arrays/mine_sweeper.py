1f2ee54d8bd569ed8fe86fb676cd5d9f23a7476c
Bart de Goede <b.de.goede@catawiki.nl>
2018-06-05 22:30:55 +0200

    Use stack to implement clearing of field

--- 2d-arrays/mine_sweeper.py
+++ 2d-arrays/mine_sweeper.py
@@ -16,6 +16,22 @@ def mine_sweeper(bombs, num_rows, num_cols):
 
     return field
 
+
+def click(field, num_rows, num_cols, x, y):
+    if field[x][y] != 0:
+        return field
+    stack = [(x, y)]
+    while stack:
+        x, y = stack.pop()
+        field[x][y] = -2
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if 0 <= i < num_rows and 0 <= j < num_cols and field[i][j] == 0:
+                    stack.append((i, j))
+
+    return field
+
+
 # It converts a 2-dimensional array (a list of lists) into
 # an easy-to-read string format.
 def to_string(given_array):
@@ -29,3 +45,16 @@ if __name__ == '__main__':
     assert(mine_sweeper([[0, 2], [2, 0]], 3, 3) == [[0, 1, -1], [1, 2, 1], [-1, 1, 0]])
     assert(mine_sweeper([[0, 0], [0, 1], [1, 2]], 3, 4) == [[-1, -1, 2, 1], [2, 3, -1, 1], [0, 1, 1, 1]])
     assert(mine_sweeper([[1, 1], [1, 2], [2, 2], [4, 3]], 5, 5) == [[1, 2, 2, 1, 0], [1, -1, -1, 2, 0], [1, 3, -1, 2, 0], [0, 1, 2, 2, 1], [0, 0, 1, -1, 1]])
+
+    field1 = [[0, 0, 0, 0, 0],
+              [0, 1, 1, 1, 0],
+              [0, 1, -1, 1, 0]]
+    assert(click(field1, 3, 5, 2, 2) == [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, -1, 1, 0]])
+
+    assert(click(field1, 3, 5, 1, 4) == [[-2, -2, -2, -2, -2], [-2, 1, 1, 1, -2], [-2, 1, -1, 1, -2]])
+
+    field2 = [[-1, 1, 0, 0],
+              [1, 1, 0, 0],
+              [0, 0, 1, 1],
+              [0, 0, 1, -1]]
+    assert(click(field2, 4, 4, 0, 1) == [[-1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 1, 1], [0, 0, 1, -1]])
